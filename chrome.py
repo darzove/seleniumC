9b207f3ecf869f7ed7395fde2d0ce8fb4914a91f
darzove <tadaspack@gmail.com>
2019-09-26 22:16:49 -0400

    finished preload for chrome. TODO: ff

--- chrome.py
+++ chrome.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from driver import SeleniumC
 from default import config
+import json
 
 class Chrome(SeleniumC):
     def __init__(self, config=config):
@@ -9,6 +10,7 @@ class Chrome(SeleniumC):
 
     def get_driver(self):
         options = webdriver.ChromeOptions()
+
         if self.config['HEADLESS']:
             options.headless = True
 
@@ -19,4 +21,20 @@ class Chrome(SeleniumC):
         if not self.config['HEADLESS'] and self.config['ADBLOCK'] and self.config['ADBLOCK_CRX'] is not None:
             options.add_extension(self.config['ADBLOCK_CRX'])
 
-        return webdriver.Chrome(chrome_options=options, executable_path=self.config['BINARY_PATH'])
+        if self.config['BINARY_PATH'] is not None:
+            return webdriver.Chrome(chrome_options=options, executable_path=self.config['BINARY_PATH'])
+        else:
+            try:
+                return webdriver.Chrome(chrome_options=options)
+            except:
+                Exception('Unable to find chromedriver, please specify the correct path to the binary in your config')
+
+    def send_command(self, command, params=[]):
+        target = f"/session/{self.driver.session_id}/chromium/send_command_and_get_result"
+        url = self.driver.command_executor._url + target
+        body = json.dumps({'cmd': command, 'params': params})
+        res = self.driver.command_executor._request('POST', url, body)
+        return res.get('value')
+
+    def inject(self, script):
+        self.send_command('Page.addScriptToEvaluateOnNewDocument', {"source": script})
--- default.py
+++ default.py
@@ -1,6 +1,6 @@
 config = {
     'BINARY_PATH': './chromedriver',
-    'ARGS': ['disable-infobars'],
+    'ARGS': ['disable-infobars', 'user-agent=Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'],
     'HEADLESS': False,
     'VIRTUAL': False, #run virtual True if you are using a box without a real monitor connected
     #widths and heights are important for window resizing as well as virtual display settings
@@ -10,5 +10,5 @@ config = {
     'JQ_URL': 'https://code.jquery.com/jquery-3.4.1.min.js',
     'ADBLOCK': True,
     'ADBLOCK_XPI': None,
-    'ADBLOCK_CRX': None
+    'ADBLOCK_CRX': './3.6.3_0.crx'
 }
